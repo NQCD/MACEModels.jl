f347cbc3a65c6bd005e4f398e1047ba906c0cded
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2025-09-11 15:52:28 +0200

    ✅ Add unit tests for MACEModels accuracy to 1e-5

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,72 @@
-using MACEModel
+using MACEModels
+using PythonCall
+using ProgressMeter
+using NQCDynamics, NQCModels, NQCBase, NQCDInterfASE
 using Test
 
-@testset "MACEModel.jl" begin
+@info "Loading python modules - if things fail here, check CondaPkg is working, and whether MACE has had any changes."
+ase_io = pyimport("ase.io")
+mc = pyimport("mace.calculators")
+#model_path = "$(@__DIR__)/test_model/MACE_WojciechDB_mySplit1_stagetwo_compiled.model"
+model_path = "$(@__DIR__)/test_model/MACE_model_swa.model"
+mace_calc_small = mc.MACECalculator(
+	model_paths=["$(@__DIR__)/test_model/MACE_model_swa.model"], 
+	device="cpu", 
+	default_dtype="float32"
+)
+ase_structure = ase_io.read("$(@__DIR__)/test_model/h2cu_diffusion_desorption_validation.xyz", index=0)
+ase_structure.calc = mace_calc_small
+mace_model_ase = ClassicalASEModel(ase_structure)
+
+@info "Checking PyTorch backends."
+torch = pyimport("torch")
+
+cuda_avail = pyconvert(Bool, torch.backends.cuda.is_built())
+mps_avail = pyconvert(Bool, torch.backends.mps.is_built())
+
+structures = NQCBase.read_extxyz("$(@__DIR__)/test_model/h2cu_diffusion_desorption_validation.xyz")
+structures_to_test = first(structures[2], 3000)
+
+@info "Available PyTorch backends:" CUDA=cuda_avail MPS=mps_avail
+
+@info "Now entering package tests."
+@testset "Model loading" begin
     # Write your tests here.
+	for backend in ["cpu", "cuda"][findall([true, cuda_avail])]
+		MACEModel(
+			structures[1],
+			structures[3],
+			[model_path];
+			default_dtype = Float32,
+			device = backend
+		)
+	end
+end
+
+@testset "Model inference" begin
+	model = MACEModel(
+		structures[1],
+		structures[3],
+		[model_path];
+		default_dtype = Float32,
+		device = "cpu",
+		batch_size = 10, # This ensures batching and un-batching operations work as well. 
+	)
+	@info "Evaluating structures using MACEModels.predict!()"
+	mace_prediction = MACEModels.predict(
+		model, 
+		structures[1],
+		structures_to_test,
+		structures[3]
+	)
+	energies_macemodels = MACEModels.get_energy_mean(mace_prediction)
+	forces_macemodels = MACEModels.get_forces_mean(mace_prediction)
+	@info "Evaluating ase calculator using NQCModels - Check NQCDInterfASE.jl if something breaks here. "
+	energies_mace = @showprogress [NQCModels.potential(mace_model_ase, pos) for pos in structures_to_test] # energies in a.u. from ASE calculator. 
+	forces_mace = @showprogress [NQCModels.derivative(mace_model_ase, pos) for pos in structures_to_test] # forces in a.u. from ASE calculator. 
+	@info "Checking equality to within 1e-5"
+	@. @test isapprox(energies_mace, energies_macemodels; atol = 1e-5)
+	for forces in zip(forces_mace, forces_macemodels)
+		@. @test isapprox(forces...; atol=1e-5)
+	end
 end
