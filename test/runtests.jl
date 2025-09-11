e9e64009c1a429b53f6a894f987dbc5e613a8b96
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2025-09-11 15:56:09 +0200

    ✅ Add CUDA unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,9 @@ using Test
 @info "Loading python modules - if things fail here, check CondaPkg is working, and whether MACE has had any changes."
 ase_io = pyimport("ase.io")
 mc = pyimport("mace.calculators")
-#model_path = "$(@__DIR__)/test_model/MACE_WojciechDB_mySplit1_stagetwo_compiled.model"
 model_path = "$(@__DIR__)/test_model/MACE_model_swa.model"
 mace_calc_small = mc.MACECalculator(
-	model_paths=["$(@__DIR__)/test_model/MACE_model_swa.model"], 
+	model_paths=[model_path], 
 	device="cpu", 
 	default_dtype="float32"
 )
@@ -65,8 +64,44 @@ end
 	energies_mace = @showprogress [NQCModels.potential(mace_model_ase, pos) for pos in structures_to_test] # energies in a.u. from ASE calculator. 
 	forces_mace = @showprogress [NQCModels.derivative(mace_model_ase, pos) for pos in structures_to_test] # forces in a.u. from ASE calculator. 
 	@info "Checking equality to within 1e-5"
-	@. @test isapprox(energies_mace, energies_macemodels; atol = 1e-5)
+	@. @test "Energies" isapprox(energies_mace, energies_macemodels; atol = 1e-5)
 	for forces in zip(forces_mace, forces_macemodels)
-		@. @test isapprox(forces...; atol=1e-5)
+		@. @test "Forces" isapprox(forces...; atol=1e-5)
+	end
+end
+
+if cuda_avail
+	mace_calc_small = mc.MACECalculator(
+		model_paths=["$(@__DIR__)/test_model/MACE_model_swa.model"], 
+		device="cuda", 
+		default_dtype="float32"
+	)
+	ase_structure = ase_io.read("$(@__DIR__)/test_model/h2cu_diffusion_desorption_validation.xyz", index=0)
+	ase_structure.calc = mace_calc_small
+	mace_model_ase = ClassicalASEModel(ase_structure)
+	model = MACEModel(
+		structures[1],
+		structures[3],
+		[model_path];
+		default_dtype = Float32,
+		device = "cuda",
+		batch_size = 25, # This ensures batching and un-batching operations work as well. 
+	)
+	@info "Evaluating structures using MACEModels.predict!()"
+	mace_prediction = MACEModels.predict(
+		model, 
+		structures[1],
+		structures_to_test,
+		structures[3]
+	)
+	energies_macemodels = MACEModels.get_energy_mean(mace_prediction)
+	forces_macemodels = MACEModels.get_forces_mean(mace_prediction)
+	@info "Evaluating ase calculator using NQCModels - Check NQCDInterfASE.jl if something breaks here. "
+	energies_mace = @showprogress [NQCModels.potential(mace_model_ase, pos) for pos in structures_to_test] # energies in a.u. from ASE calculator. 
+	forces_mace = @showprogress [NQCModels.derivative(mace_model_ase, pos) for pos in structures_to_test] # forces in a.u. from ASE calculator. 
+	@info "Checking equality to within 1e-5"
+	@. @test "Energies" isapprox(energies_mace, energies_macemodels; atol = 1e-5)
+	for forces in zip(forces_mace, forces_macemodels)
+		@. @test "Forces" isapprox(forces...; atol=1e-5)
 	end
 end
