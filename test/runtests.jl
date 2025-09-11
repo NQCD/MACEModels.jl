be18c80bfef5779a0b7f2d9219bd741d3cf03fce
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2025-09-11 17:39:23 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -69,9 +69,9 @@ end
 	energies_mace = @showprogress [NQCModels.potential(mace_model_ase, pos) for pos in structures_to_test] # energies in a.u. from ASE calculator. 
 	forces_mace = @showprogress [NQCModels.derivative(mace_model_ase, pos) for pos in structures_to_test] # forces in a.u. from ASE calculator. 
 	@info "Checking equality to within 1e-5"
-	@. @test "Energies" isapprox(energies_mace, energies_macemodels; atol = 1e-5)
+	@. @test isapprox(energies_mace, energies_macemodels; atol = 1e-5)
 	for forces in zip(forces_mace, forces_macemodels)
-		@. @test "Forces" isapprox(forces...; atol=1e-5)
+		@. @test isapprox(forces...; atol=1e-5)
 	end
 end
 
