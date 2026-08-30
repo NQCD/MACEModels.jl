a24955fd9a92bbe94e7bb456fe516ec932caac55
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2026-08-30 09:38:07 +0200

    Oopsfor

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,7 +20,7 @@ end
 mps_avail = get(ENV, "JULIA_MACEMODELS_TEST_MPS", pyconvert(Bool, torch.backends.mps.is_built()))
 
 backends = ["cpu", "cuda", "mps"] # ["cpu", "cuda", "mps"]
-backends_avail = [cuda_avail, mps_avail] # [true, cuda_avail, mps_avail]
+backends_avail = [true, cuda_avail, mps_avail] # [true, cuda_avail, mps_avail]
 if cuda_avail
     using CUDA
 end
